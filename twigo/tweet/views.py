3fb2c56a473af903baaed2aea68e7fdfd3ecac7e
mosafdar <mosafdaralii@gmail.com>
2024-07-17 15:07:10 +0500

    Modified the codebase to improve the functionality of jinja templating

--- twigo/tweet/views.py
+++ twigo/tweet/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
 
 from .models import TweetModel
@@ -24,7 +25,7 @@ def create_twigo(request):
     return render(request, 'index.html', {"form": form})
 
 def update_twigo_post(request, twigo_id):
-    twigo_post = get_object_or_404(TweetModel, pk=twigo_id, user = request.user)
+    twigo_post = get_object_or_404(TweetModel, pk=twigo_id, user=request.user)
     if request.method == "POST":
         form = TweetForm(request.POST, request.FILES, instance=twigo_post)
         if form.is_valid():
@@ -37,10 +38,10 @@ def update_twigo_post(request, twigo_id):
     return render(request, 'tweet/index.html', {"form": form})
 
 def delete_twigo_post(request, twigo_id):
-    twigo_post = get_object_or_404(TweetModel, pk=twigo_id, user = request.user)
+    twigo_post = get_object_or_404(TweetModel, pk=twigo_id, user=request.user)
     if request.method == "POST":
         twigo_post.delete()
         return redirect('twigos_home')
     else:
         form = TweetForm(instance=twigo_post)
-    return render(request, 'tweet/index.html', {"form": form})
+    return render(request, 'tweet/delete_confirm_page.html', {"form": form})
