8b0e681f4b1633b35119430bb11916f37fdfaa53
mosafdar <mosafdaralii@gmail.com>
2024-07-26 03:35:41 +0500

    vercel sol1.0

---
+++ build_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Build the project
+echo "Building the project..."
+python3.12 -m pip install -r requirements.txt
+
+# making migrations for databases
+echo "Make Migration..."
+python3.12 twigo/manage.py makemigrations --noinput
+python3.12 twigo/manage.py migrate --noinput
+
+# collecting static files
+echo "Collect Static..."
+python3.12 manage.py collectstatic --noinput --clear
