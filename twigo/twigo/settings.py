ecf326ab2cee4dffeaf2c56bd76f95285b026071
mosafdar <mosafdaralii@gmail.com>
2024-07-26 03:41:04 +0500

    .

--- twigo/twigo/settings.py
+++ twigo/twigo/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
         os.path.join(BASE_DIR, 'theme/static')
