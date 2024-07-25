8fa6e63dd8feb428e4556306fcab9a71783d3e34
mosafdar <mosafdaralii@gmail.com>
2024-07-26 03:57:58 +0500

    .

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+   #!/bin/bash
 
-# Build the project
-echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+   # Build the project
+   echo "Building the project..."
+   python3.12 -m pip install -r requirements.txt
 
-# making migrations for databases
-echo "Make Migration..."
-python3.12 twigo/manage.py makemigrations --noinput
-python3.12 twigo/manage.py migrate --noinput
+   # making migrations for databases
+   echo "Make Migration..."
+   python3.12 twigo/manage.py makemigrations --noinput
+   python3.12 twigo/manage.py migrate --noinput
 
-# collecting static files
-echo "Collect Static..."
-python3.12 twigo/manage.py collectstatic --noinput --clear
+   # collecting static files
+   echo "Collect Static..."
+   python3.12 twigo/manage.py collectstatic --noinput --clear --output staticfiles_build/static
