f644fce8265339f316180970ccca4d0c59ca63d3
mosafdar <mosafdaralii@gmail.com>
2024-07-26 02:10:18 +0500

    vercel setup integrated

--- twigo/twigo/settings.py
+++ twigo/twigo/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
-
+import dj_database_url
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-27x%-n*ty!9sn65&y_a@o-_%h1@d&_7&53ab6j7kqrra637ab1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*.vercel.app", "localhost", "127.0.0.1", '.now.sh']
 
 
 # Application definition
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_browser_reload.middleware.BrowserReloadMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'twigo.urls'
@@ -74,7 +76,11 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'theme/static')
+    ]
 
 WSGI_APPLICATION = 'twigo.wsgi.application'
 
@@ -83,11 +89,19 @@ WSGI_APPLICATION = 'twigo.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': dj_database_url.config(default=os.envrion.get('TWIGO_DATABASE_URL'))
+    {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Twigo_app',
+        'USER': 'postgres',
+        'PASSWORD': 'mosafdar@123', # use environment variable to get these credentials
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
+# Configuring the default connection
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
@@ -123,8 +137,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
